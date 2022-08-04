86a151a6edb0f59c8b1cb2b09a04ee8c3cedcbb8
Gabriel <gabrielvrsas@hotmail.com>
2022-08-04 16:58:20 -0300

    Update dataset analysis

--- new_app.py
+++ new_app.py
@@ -120,26 +120,26 @@ elif options == 'Dataset Analysis':
 	col1, col2 = st.columns([4, 4])
 	with col1: 
 		fig_3 = px.density_heatmap(df, x = 'Diabetes_012', y = 'GenHlth')
-		fig_3.update_layout(y_axis = dict(tickmode = 'array', tickvals = [1, 2, 3, 4, 5],
-							ticktext = ['Excellent', 'Very good', 'Good', 'Fair', 'Poor']), 
-							title_text = 'Percentage of GenHlth Level by<br> Class')	
+		#fig_3.update_layout(y_axis = dict(tickmode = 'array', tickvals = [1, 2, 3, 4, 5],
+		#					ticktext = ['Excellent', 'Very good', 'Good', 'Fair', 'Poor']), 
+		#					title_text = 'Percentage of GenHlth Level by<br> Class')	
 		st.plotly_chart(fig_3)
 	with col2: 
 		fig_4 = px.density_heatmap(df, x = 'Diabetes_012', y = 'Education')
-		fig_4.update_layout(y_axis = dict(tickmode = 'array', tickvals = [1, 2, 3, 4, 5, 6],
-							ticktext = ['Never attended school / kindergarten', 'Elementary', 
-							'Some high school', 'High school graduate', 'Some college or technical school', 
-							'College graduate']), 
-							title_text = 'Percentage of Education Level by<br> Class')	
+		#fig_4.update_layout(y_axis = dict(tickmode = 'array', tickvals = [1, 2, 3, 4, 5, 6],
+		#					ticktext = ['Never attended school / kindergarten', 'Elementary', 
+		#					'Some high school', 'High school graduate', 'Some college or technical school', 
+		#					'College graduate']), 
+		#					title_text = 'Percentage of Education Level by<br> Class')	
 		st.plotly_chart(fig_4)
 	col1, col2 = st.columns([2.2, 5.8])
 	with col2:
 		fig_5 = px.density_heatmap(df, x = 'Diabetes_012', y = 'Income')
-		fig_5.update_layout(y_axis = dict(tickmode = 'array', tickvals = [1, 2, 3, 4, 5, 6, 7, 8],
-							ticktext = ['Less than $10,000', '$10,000 to less than $15,000', 
-							'$15,000 to less than $20,000', '$20,000 to less than $25,000', '$25,000 to less than $35,000', 
-							'$35,000 to less than $50,000', '$50,000 to less than $75,000', '$75,000 or more']), 
-							title_text = 'Percentage of Income Level by<br> Class')	
+		#fig_5.update_layout(y_axis = dict(tickmode = 'array', tickvals = [1, 2, 3, 4, 5, 6, 7, 8],
+		#					ticktext = ['Less than $10,000', '$10,000 to less than $15,000', 
+		#					'$15,000 to less than $20,000', '$20,000 to less than $25,000', '$25,000 to less than $35,000', 
+		#					'$35,000 to less than $50,000', '$50,000 to less than $75,000', '$75,000 or more']), 
+		#					title_text = 'Percentage of Income Level by<br> Class')	
 		st.plotly_chart(fig_5)
 	st.markdown("""<p style='text-align: center; color: black;'>
 
